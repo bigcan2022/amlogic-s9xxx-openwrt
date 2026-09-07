cde4dc0666dde9ab066ce02796fe9087f022fed9
bigcan2022 <105194911+bigcan2022@users.noreply.github.com>
2026-09-07 10:10:29 +0800

    Bundle latest ARM64 Mihomo core for offline first boot

--- config/immortalwrt_master/diy-part1.sh
+++ config/immortalwrt_master/diy-part1.sh
@@ -13,5 +13,17 @@
 rm -rf package/luci-app-openclash
 git clone --depth 1 --branch master https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 
+# Bundle the latest official ARM64 Meta/Mihomo core so OpenClash can start
+# before the box has working proxy access.
+openclash_core_dir="package/luci-app-openclash/luci-app-openclash/root/etc/openclash/core"
+openclash_core_tmp="$(mktemp -d)"
+mkdir -p "${openclash_core_dir}"
+curl -fL --retry 3 \
+    "https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz" \
+    -o "${openclash_core_tmp}/clash-linux-arm64.tar.gz"
+tar -xzf "${openclash_core_tmp}/clash-linux-arm64.tar.gz" -C "${openclash_core_tmp}"
+install -m 0755 "${openclash_core_tmp}/clash" "${openclash_core_dir}/clash_meta"
+rm -rf "${openclash_core_tmp}"
+
 # Remove unnecessary packages
 # rm -rf package/emortal/{autosamba,ipv6-helper}
